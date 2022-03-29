0baef9a770fb71b805a1deec340e46e9b6ecc5ec
namansigh3502 <namansingh0305@gmail.com>
2022-03-29 10:46:12 +0530

    Java Programs

--- UDPClient.java
+++ UDPClient.java
@@ -3,22 +3,16 @@ import java.net.*;
 
 public class UDPClient{
 
-    public static void main(String[] args){
-        DatagramSocket socket = null;
-        String str;
-        try{
-            socket = new DatagramSocket(3000);
-            byte[] b = new byte[1000];
+    public static void main(String[] args) throws Exception{
+        DatagramSocket socket = new DatagramSocket(3000);
+        byte[] b = new byte[1000];
+        DatagramPacket reply = new DatagramPacket(b,b.length);
 
-            while(true){
-                DatagramPacket reply = new DatagramPacket(b,b.length);
-                socket.receive(reply);
-                str = new String(reply.getData(),0,reply.getLength());
-                System.out.println( "client received: " + str );
-            }
+        while(true){
+            socket.receive(reply);
+            String str = new String(reply.getData(),0,reply.getLength());
+            System.out.println( "client received: " + str );
         }
-        catch(Exception ex){
-            System.out.println(ex);
-        }
+
     }
 }
